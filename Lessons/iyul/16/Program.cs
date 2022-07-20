87172dcca79b41f49ccb8eeb7e1fc38aac913fbe
AzerH <30517450+AzerH@users.noreply.github.com>
2022-07-20 14:32:17 +0400

    ders 16 iyun

---
+++ Lessons/iyul/16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Azer";
+            
+
+            student.ShowEmail();
+        }
+    }
+}
