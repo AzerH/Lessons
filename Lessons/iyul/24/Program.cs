e4820887c0430ad373c360f0fc269faf9d402d82
AzerH <30517450+AzerH@users.noreply.github.com>
2022-07-26 10:32:10 +0400

    ders 24 iyul

---
+++ Lessons/iyul/24/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24iyul
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Customer customer1 = new Customer();
+            customer1.Name = "Azer";
+            customer1.Surname = "Hasanov";
+            customer1.Password = "Azer7778";
+            customer1.Username = "Duman";
+            customer1.Email = "azer.hasanov@gmail.com";
+
+            Customer.CheckCustomer(customer1);
+            Customer.ShowDb();
+            Console.ReadLine();         
+
+        }
+
+    }
+
+}
